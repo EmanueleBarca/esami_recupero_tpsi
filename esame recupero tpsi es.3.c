cefc8fd425e5c17e55d0442e54de191e7309aa30
EmanueleBarca <emanuele.barca@davincimilazzo.edu.it>
2025-08-26 12:31:13 +0200

    Add files via upload

---
+++ esame recupero tpsi es.3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int somma_vettore(int v[]);
+
+int main(){
+    int vet[10]={1,2,3,4,5,6,7,8,9,10};
+
+    
+}
+
+int somma_vettore(int v[]){
+    int somma = 0;
+    for (int i = 0, i<10, i++)
+}
